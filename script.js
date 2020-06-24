84d7a4610517d89f05e3b98d06985301c3b597dc
Cody Samuels <twowoodenspoons@gmail.com>
2020-06-24 12:42:52 -0700

    Merged Andrew's code

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+// we want a random hike generated, 
+// Then recieve hike input/ first api call from var randomHike .
+// then we want the weather API to grab hikes lat and lon information 
+// After weather API gathers lat and lon, 
+// then display 6 hour forecast broken up into 3 displays
+// Display .sunrise, .sunset, .Temp, .Wind, .Description 
+
+
 // GLOBAL VARIABLE & OJBJECTS
-var HikingProjectAPIDataObject = {}
+
+var hikingProjectAPIDataObject = {}
+var weatherForecastObject = {}
 var userParameters = {
     latitude: ["lat=", "47.6062"],
     longitude: ["&lon=", "-122.3321"],
@@ -27,7 +37,7 @@ function queryHikingProjectDataAPI() {
         method: "GET"
     })
         .then(function (hikingAPIResponse) {
-            HikingProjectAPIDataObject = hikingAPIResponse
+            hikingProjectAPIDataObject = hikingAPIResponse
             getRandomTrail()
         });
 }
@@ -37,6 +47,53 @@ function getRandomTrail() {
     var randomNum = ""
     randomNum = Math.floor(Math.random() * userParameters.maxResults[1] + 1)
     randomTrailObject = HikingProjectAPIDataObject.trails[randomNum]
-    lat = (randomTrailObject.longitude)
-    long = (randomTrailObject.latitude)
+    lat = (randomTrailObject.latitude)
+    long = (randomTrailObject.longitude)
+    console.log(HikingProjectAPIDataObject.trails[randomNum])
+}
+
+// //  AJAX FUNCTION FOR WEATHER LAT LON QUERY---STILL NEED TO INTEGRATE WITH HIKING PROJECT DATA API
+
+// function weatherLocation() {
+//     $.ajax({
+//         url: weatherQuery,
+//         method: "GET"
+//     })
+//         .then(function (weatherCoord) {
+//             console.log(weatherCoord)
+
+//             var trailLat = weatherCoord.city.coord.lat;
+//             var trailLon = weatherCoord.city.coord.lon
+
+            // weatherQuery = `https://api.openweathermap.org/data/2.5/onecall?lat=${trailLat}&lon=${trailLon}&exclude=minutely&appid=${APIkey}`
+
+//         })
+// }
+// // 6 HOUR FORECAST USING THE CURRENT TIME. SPLIT INTO 3 TWO HOUR BLOCKS. 
+// // HOW DO GET THE CALL TO RECOGNIZE CURRENT TIME AND THEN DISPLAY THE FOLLOWING 6 HOURS?
+
+
+function sixHourForecast() {
+    var weatherAPIKey = "4af87ee91531ff09b1ce9e3392587b3a"
+    var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&appid=${weatherAPIKey}`
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+
+    })
+        // TWO HOUR TIME BLOCKS WILL BE LOOKED AT OVER A 6 HOUR TIME PERIOD
+        .then(function (twoHourBlock) {
+
+            weatherForecastObject = twoHourBlock
+            var twoHour = []
+
+            for (var i = 0; i < twoHourBlock.list.length; i++) {
+
+                if (twoHourBlock.list[i].dt_txt.indexOf("12:00:00") !== -1) {
+                    console.log(twoHourBlock.list[i])
+                }
+
+            }
+        })
 }
