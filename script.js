de6b9917a25936b29acee3d38fe6d4b557612f6c
Cody Samuels <twowoodenspoons@gmail.com>
2020-06-25 15:23:55 -0700

    Merged Ricky's beautiful map function

--- script.js
+++ script.js
@@ -5,7 +5,9 @@
 // then display 6 hour forecast broken up into 3 displays
 // Display .sunrise, .sunset, .Temp, .Wind, .Description 
 // GLOBAL VARIABLE & OJBJECTS
+
 var hikingProjectAPIDataObject = JSON.parse(localStorage.getItem("hikesData")) || {};
+var randomTrailObject = JSON.parse(localStorage.getItem("randomHikeData")) || {};
 var weatherForecastObject = {}
 var randomTrailObject = {}
 var userParameters = {
@@ -17,7 +19,7 @@ var userParameters = {
     minLength: ["&minLength=", ""],
     minStars: ["&minStars=", ""]
 };
-
+var map;
 var lat = ""
 var long = ""
 
@@ -26,6 +28,7 @@ function queryHikingProjectDataAPI() {
     // VARIABLES SPECIFIC TO THE FUNCTION
     var hikingProjectAPIKey = "&key=200805406-750e5250addc429fbca823b830432e1f"
     var hikingQueryURL = "https://www.hikingproject.com/data/get-trails?" + userParameters.latitude[0] + userParameters.latitude[1] + userParameters.longitude[0] + userParameters.longitude[1] + userParameters.maxDistance[0] + userParameters.maxDistance[1] + userParameters.maxResults[0] + userParameters.maxResults[1] + userParameters.minLength[0] + userParameters[1] + userParameters.minStars[0] + userParameters.minStars[1] + hikingProjectAPIKey
+
     // AJAX FUNCTION
     $.ajax({
         url: hikingQueryURL,
@@ -33,9 +36,11 @@ function queryHikingProjectDataAPI() {
     })
         .then(function (hikingAPIResponse) {
             hikingProjectAPIDataObject = hikingAPIResponse
+            // getRandomTrail()
+            // initMap(queryURL)
+            fisherYatesShuffle(hikingProjectAPIDataObject.trails)
             localStorage.setItem("hikesData", JSON.stringify(hikingProjectAPIDataObject))
-            getRandomTrail()
-            initMap(queryURL)
+            console.log(hikingProjectAPIDataObject)
         });
 }
 
@@ -49,82 +54,55 @@ function getRandomTrail() {
 }
 
 // GOOGLE MAPS FUNCTION
-function initMap(queryURL) {
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        var trailsAR = response.trails
-        var randomTrail = trailsAR[getRandomTrail()];
-        console.log(randomTrail);
+function initMap() {
+    lat = randomTrailObject.latitude
+    lng = randomTrailObject.longitude
+    var loc = randomTrailObject.location
+    var sum = randomTrailObject.summary 
+    var con = randomTrailObject.conditionDetails
+    var img = randomTrailObject.imgSqSmall
+    var myLatLng = { lat, lng };
 
-        var lat = randomTrail.latitude
-        var lng = randomTrail.longitude
-        var loc = randomTrail.location
-        var sum = randomTrail.summary
-        var con = randomTrail.conditionDetails
-        var img = randomTrail.imgSqSmall
-        console.log(loc);
-        console.log(sum);
-        console.log(con);
-        console.log(response);
-        $(".welcomeButtons").click(function () {
-
-
-        })
-
-       
-
-
-
-
-        var myLatLng = { lat, lng };
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 12,
-            center: myLatLng
-        });
-        var contentString = '<div id="content">' +
-            '<div id="siteNotice">' +
-            '</div>' +
-            '<h1 id="firstHeading" class="firstHeading">Enjoy your Hike.</h1>' +
-            '<div id="bodyContent">' +
-            '<h1>' + loc + '</h1>' +
-            '<h2>' + sum + '</h2>' +
-            '<h3>' + con + '</h3>' +
-            '</div>' +
-            '</div>';
-
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString
-        })
-
-        var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            title: 'Uluru (Ayers Rock)'
-        });
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
-    });
+      map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: lat, lng: lng },
+        zoom: 12
+      });
+      var contentString = '<div id="content">' +
+      '<div id="siteNotice">' +
+      '</div>' +
+      '<h1 id="firstHeading" class="firstHeading">Enjoy your Hike.</h1>' +
+      '<div id="bodyContent">' +
+      '<h1>'+loc+'</h1>' +
+      '<h2>'+sum+'</h2>'+
+      '<h3>'+ con + '</h3>' +  
+      '</div>' +
+      '</div>';
+      var infowindow = new google.maps.InfoWindow({
+          content: contentString})
+          var marker = new google.maps.Marker({
+              position: myLatLng,
+              map: map,
+            });
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+            
+            //       console.log(randomTrailObject); 
+            
+            //       console.log(loc);
+            //       console.log(sum);
+            //       console.log(con);
+            //       console.log(response);
+            //       // $("#button").click(function () {
+                //       // })
+                //     var map = new google.maps.Map(document.getElementById('hikeMap'), {
+                    //       zoom: 12,
+                    //       center: myLatLng
+                    //     });
+                    
+                    //  }); 
+                    // }
 }
-
-//TIME CONVERTER FUNCTION
-function timeConverter(UNIX_timestamp){
-    var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-    return time;
-  }
-//   console.log(timeConverter(weatherForecastObject.current.sunrise));
-
 
 //  FORECAST FUNCTION
 function sixHourForecast() {
@@ -189,6 +167,7 @@ function populateRandomPage() {
     // FOR ADDING ANDREW'S WEATHER
     // $("#weather").text(randomTrailObject.name)
 }
+
 // POPULATES THE LIST PAGE
 function populateListPage() {
     for (let i = 0; i < 10; i++) {
@@ -207,6 +186,7 @@ function populateListPage() {
         })
     }
 }
+
 // POPULLATES SELECTION PAGE
 function populateSelectionPage() {
     // CONVERTS DIFFICULTY TO SOMETHING EASIER TO READ
@@ -231,4 +211,23 @@ function populateSelectionPage() {
     $("#hikeDescription").text(hikingProjectAPIDataObject.trails[99].summary)
     // FOR ADDING ANDREW'S WEATHER
     // $("#weather").text(randomTrailObject.name)
+}
+
+// FISHER-YATES SHUFFLE
+function fisherYatesShuffle(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
 }
