d9b164f84a89e740f980567746e6b1bfca6b756e
Enrique Garcia <rickyg218@gmail.com>
2020-06-26 11:45:30 -0700

    multiple pins working

--- script.js
+++ script.js
@@ -165,19 +165,19 @@ function initMapSelection() {
 // POPULATES THE RANDOM PAGE
 function multiplepins() {
     console.log(hikingProjectAPIDataObject);
-    // var locations = []
+    var locations = []
    
-    // for (var i = 0; i < 10; i++) {
-    //     var newArr = [hikingProjectAPIDataObject.trails[i].name, hikingProjectAPIDataObject.trails[i].latitude, hikingProjectAPIDataObject.trails[i].longitude, i]
-    //     locations.push(newArr)
-    // }
-    var locations = [
-        ['Bondi Beach', -33.890542, 151.274856, 4],
-        ['Coogee Beach', -33.923036, 151.259052, 5],
-        ['Cronulla Beach', -34.028249, 151.157507, 3],
-        ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-        ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];
+    for (var i = 0; i < 10; i++) {
+        var newArr = [hikingProjectAPIDataObject.trails[i].name, hikingProjectAPIDataObject.trails[i].latitude, hikingProjectAPIDataObject.trails[i].longitude, i]
+        locations.push(newArr)
+    }
+    // var locations = [
+    //     ['Bondi Beach', -33.890542, 151.274856, 4],
+    //     ['Coogee Beach', -33.923036, 151.259052, 5],
+    //     ['Cronulla Beach', -34.028249, 151.157507, 3],
+    //     ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+    //     ['Maroubra Beach', -33.950198, 151.259302, 1]
+    // ];
     console.log(locations);
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
